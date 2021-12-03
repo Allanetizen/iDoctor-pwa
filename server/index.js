4d6f2e56d0e7fb5b9114ef83498ec17d3d48f843
allanetizenm <allankosgei747@gmail.com>
2021-12-03 15:58:18 +0300

    connection string

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
-
 import bodyParser from "body-parser";
-import  Mongoose  from "mongoose";
-import  Express  from "express";
-import cors from 'cors';
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "";
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+mongoose.set("useFindAndModify", false);
