da8c0a06f1b10809a3809b025e3c52024305358a
allanetizenm <allankosgei747@gmail.com>
2021-12-04 15:44:12 +0300

    gitignore file & connection string

--- server/index.js
+++ server/index.js
@@ -1,18 +1,37 @@
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import express from "express";
-import cors from "cors";
-const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-
-const CONNECTION_URL = "";
-const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
-mongoose.set("useFindAndModify", false);
+(async () => {
+    const express = require("express");
+    const cors = require("cors");
+    const bodyParser = require('body-parser');
+    // const morgan = require('logger');
+    const routes = require("./routes/posts");
+  
+  
+    require("dotenv").config();
+    const app = express();
+    const port = process.env.PORT || 5000;
+  
+    app.use(cors());
+    app.use(express.json());
+    
+    app.use(bodyParser.urlencoded({
+      extended: false
+    }));
+    app.use(bodyParser.json());
+    
+    // app.use(morgan('dev'));
+    // get driver connection
+    const dbo = require("./db/conn").default;
+  
+    try {
+      const db = await dbo.connectToServer();
+  
+      app.use(routes(db));
+  
+      app.listen(port, () => {
+        // perform a database connection when server start
+        console.log(`Server is running on port: ${port}`);
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  })();
